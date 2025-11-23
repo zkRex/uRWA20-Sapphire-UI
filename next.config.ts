54de4c40c960671c013c8cc10c0c8851cefe221b
devbotbo <7118.eth@proton.me>
2025-11-23 08:15:04 -0300

    Add webpack configuration to next.config.ts for aliasing and null-loader support, improving module resolution and handling of test files in node_modules.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'why-is-node-running': false,
+    };
+
+    config.module.rules.push({
+      test: /node_modules\/.*\/test\//,
+      loader: 'null-loader',
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
